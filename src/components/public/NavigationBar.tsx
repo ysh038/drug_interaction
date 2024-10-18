7a1c44e4981954d298a1c153d8c30307f5e93b22
ysh038 <ysh038@naver.com>
2024-10-18 16:23:47 +0900

    nb

---
+++ src/components/public/NavigationBar.tsx
@@ -0,0 +1,13 @@
+
+
+function NavigationBar(){
+    return(
+        <>
+            <div>
+                <h2 className={`text-3xl`}>네비게이션 바 입니다.</h2>
+            </div>
+        </>
+    )
+}
+
+export default NavigationBar
