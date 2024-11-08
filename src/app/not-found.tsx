1587a8b860395329f08338bdb295a8710f3ea468
ysh038 <ysh038@naver.com>
2024-11-09 00:02:35 +0900

    update

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function NotFound() {
+  return (
+    <div className={`text-center`}>
+      <h1 className={`text-5xl`}>Not Found Page!</h1>
+    </div>
+  );
+}
